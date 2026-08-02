6872a0255c7f0c02155c293d7f1a490a6bc00bc8
lpearf-pixel <lpearf@gmail.com>
2026-08-02 19:48:31 +0800

    feat(research): load immutable source inventory

---
+++ apps/star-omen/src/research_sources/__init__.py
@@ -0,0 +1,11 @@
+from .source_inventory import (
+    SourceInventory,
+    SourceInventoryError,
+    load_source_inventory,
+)
+
+__all__ = [
+    "SourceInventory",
+    "SourceInventoryError",
+    "load_source_inventory",
+]
